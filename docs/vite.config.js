a734bafc78a67ffe35bfc5cb7310b6b8232a2575
Jeff Leach <jeffrey.d.leach@jpl.nasa.gov>
2025-07-22 10:41:16 -0500

    More doc updates

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/unity-terraform-modules/',
+  base: '/custom-mc-modules/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: 'index.html'
+      }
+    }
   },
   server: {
     port: 3000,
     open: true
-  }
+  },
+  publicDir: '.'
 })
